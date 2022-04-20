0086d82a043a3a0e99c953181f22c9ccb45bda1c
GuyAzoulay <87694635+GuyAzoulay@users.noreply.github.com>
2022-04-20 17:44:39 +0300

    Update stack.hpp

--- stack.hpp
+++ stack.hpp
@@ -1,17 +1,36 @@
 #include <iostream>
 #include <cstdlib>
-#include <unistd.h>
-#include "malloc.hpp"
+#include <pthread.h>
+//#include <mutex>
+//#include "mymalloc.hpp"
 using namespace std;
+
 
 
 
 //stack class implementation linked list
 template <typename T> class stack {
+  // private:
+    typedef struct node {
+      T data;
+      size_t size;
+      bool is_free;
+      node *next;
+      
+      
+    };
+    node *head;
+    node *start;
+    node *end1;
+    pthread_mutex_t lock;
+  
   public:
+
     stack() {
-      //node *check= (node *)malloc(sizeof(node)); 
-      node *check = (node*) my_malloc(sizeof(node)); 
+      // unsigned long size = sizeof(node);
+      // cout << "size of node is " << size << endl;
+      //node *check= (node *)my_malloc(sizeof(node)); 
+      node *check= (node*)my_malloc(sizeof(node)); 
       if (check==NULL)
       {
        cout << "Null pointer has been returned" << endl;
@@ -23,13 +42,15 @@ template <typename T> class stack {
       while(head != NULL) {
         node *temp = head;
         head = head->next;
-        delete temp;
+        //delete temp;
+      free(temp);
+      
       }
     }
     void push(const T& new_data) {
-      //node *check= (node*)malloc(sizeof(node));
-      node *check = (node *)my_malloc(sizeof(node));
       
+      //node *check= (node*)my_malloc(sizeof(node));
+      node *check= (node*)my_malloc(sizeof(node));
       if (check==NULL)
       {
        cout << "Null pointer has been returned" << endl;
@@ -39,6 +60,9 @@ template <typename T> class stack {
       node *temp = new node;
       temp->data = new_data;
       temp->next = head;
+      temp->is_free = false;
+      temp->size = sizeof(node);
+      // update the other variables!!!!!!!!!
       head = temp;
     }
     void pop() {
@@ -56,12 +80,59 @@ template <typename T> class stack {
     bool empty() const {
       return head == NULL;
     }
-  private:
-    struct node {
-      T data;
-      node *next;
-    };
-    node *head;
-};
 
+  
+
+    node *look_for_memory(size_t size){
+      node *temp = start;
+      while(temp != NULL) {
+        if(temp->is_free && temp->size >= size)
+          return temp;
+        temp = temp->next;
+      }
+        return (NULL);
+    }
+    node *update_node(node *nnode, size_t size, void *memory_block){
+      nnode = (node*)memory_block;
+      nnode->size = size;
+      nnode->is_free = false;
+      nnode->next = NULL;
+      if(!start)
+        start = nnode;
+      if(end1)
+        end1->next = nnode;
+      end1 = nnode;
+      pthread_mutex_unlock(&lock);
+      return (nnode);
+    }
+
+      void * my_malloc(unsigned long size){
+        cout <<"Amen"<<endl;
+        void *memory_block;
+        node *header;
+        pthread_mutex_lock(&lock);
+        if(!size)
+          return (NULL);
+        header = look_for_memory(size);
+        if(header != NULL) {
+          header->is_free = false;
+          pthread_mutex_unlock(&lock);
+          return (void*)(header + 1);
+        }
+        memory_block = sbrk((sizeof(node) + size));
+        if(memory_block == (void*) - 1) {
+          pthread_mutex_unlock(&lock);
+          return (NULL);
+        }
+        header = update_node(header, size, memory_block);
+        return (void*)(header + 1);
+      }
+        
+      
+    
+
+
+
+ 
+};
 
