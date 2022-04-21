27167419dec9f82612027a0121389be060f265cd
GuyAzoulay <87694635+GuyAzoulay@users.noreply.github.com>
2022-04-21 13:51:02 +0300

    Add files via upload

--- stack.hpp
+++ stack.hpp
@@ -1,190 +1,194 @@
 #include <iostream>
 #include <cstdlib>
 #include <pthread.h>
-//#include <mutex>
-//#include "mymalloc.hpp"
 using namespace std;
 
 
 
 
-//stack class implementation linked list
+//stack class implementation 
 template <typename T> class stack {
   // private:
-    struct node {
-      T data;
-      size_t size;
-      bool is_free;
-      node *next;
+     struct node {
+      T data;      // variable which holds the data
+      size_t size; // variable which holds the size of the data
+      bool is_free; // variable which holds the status of the node
+      node *next; // variable which holds the address of the next node
       
       
     };
-    node *head;
-    node *start;
-    node *end1;
-    pthread_mutex_t lock;
+    node *head; // variable which holds the address of the head node
+    node *start; // variable which holds the address of the start node
+    node *end1;  // variable which holds the address of the end node
+    pthread_mutex_t lock; // variable which holds the lock
   
   public:
 
+
+    // constructor of the stack class
     stack() {
-      // unsigned long size = sizeof(node);
-      // cout << "size of node is " << size << endl;
-      //node *check= (node *)my_malloc(sizeof(node)); 
-      node *check= (node*)my_malloc(sizeof(node)); 
-      if (check==NULL)
+     
+      node *check= (node*)my_malloc(sizeof(node)); // allocate memory for the head node using my_malloc function
+      if (check==NULL)                         // if the allocation fails
       {
-       cout << "Null pointer has been returned" << endl;
-      }
-      
+        cout<<"Memory allocation failed"<<endl;    // print the error message
+          }
       head = NULL;
     }
+
+    // destructor of the stack class
     ~stack() {
-      while(head != NULL) {
+      while(head != NULL) {  // while the head node is not NULL 
         node *temp = head;
         head = head->next;
-        //delete temp;
-      free(temp);
+    
+       my_free(temp);
       
       }
     }
+
+    // this function pushes the data to the stack
     void push(const T& new_data) {
       
-      //node *check= (node*)my_malloc(sizeof(node));
-      node *check= (node*)my_malloc(sizeof(node));
+      node *check= (node*)my_calloc(1,sizeof(node)); // allocate memory for the new node using my_calloc function
+
       if (check==NULL)
       {
        cout << "Null pointer has been returned" << endl;
       }
       
-      cout << " inside push-stack" << endl;
-      node *temp = new node;
-      temp->data = new_data;
-      temp->next = head;
-      temp->is_free = false;
-      temp->size = sizeof(node);
-      // update the other variables!!!!!!!!!
-      head = temp;
+      cout << " inside push-stack" << endl; //self-checking
+      node *temp = new node; // create a new node
+      temp->data = new_data; // assign the data to the new node
+      temp->next = head;   // assign the next node to the head node
+      temp->is_free = false; // assign the status of the node to false
+      temp->size = sizeof(node); // assign the size of the node to the size of the node
+      head = temp; // assign the head node to the new node
     }
+
+    // this function pops the data from the stack
     void pop() {
-        cout << " inside pop-stack" << endl;
+        cout << " inside pop-stack" << endl; //self-checking
       if(head != NULL) {
-        node *temp = head;
-        head = head->next;
+        node *temp = head; // create a new node
+        head = head->next; // assign the next node to the head node
         //delete temp;
-        my_free((void*)temp);
+        my_free((void*)temp); // free the memory of the node using my_free function
       }
     }
+
+    // this function returns the top element of the stack
     T top() const {
       return head->data;
     }
+    // this function returns if the stack is empty or not
     bool empty() const {
       return head == NULL;
     }
 
   
-
+    // this function looks for new memory by giving the size of the memory we need to allocate 
     node *look_for_memory(size_t size){
       node *temp = start;
-      while(temp != NULL) {
-        if(temp->is_free && temp->size >= size)
+      while(temp != NULL) {  // while the start node is not NULL
+        if(temp->is_free && temp->size >= size) // checking the status of the node (if it is free) and the size of the node (if it is greater than or equal to the size we need)
           return temp;
-        temp = temp->next;
+        temp = temp->next; 
       }
         return (NULL);
     }
+
+    // this function updates all node is variables 
     node *update_node(node *nnode, size_t size, void *memory_block){
-      nnode = (node*)memory_block;
+      nnode = (node*)memory_block; 
       nnode->size = size;
-      nnode->is_free = false;
-      nnode->next = NULL;
-      if(!start)
+      nnode->is_free = false; // assign the status of the node to false
+      nnode->next = NULL; 
+      if(!start) // if the start node is NULL
         start = nnode;
-      if(end1)
+      if(end1) // if the end node is not NULL
         end1->next = nnode;
       end1 = nnode;
-      pthread_mutex_unlock(&lock);
+      pthread_mutex_unlock(&lock); // unlock the mutex
       return (nnode);
     }
-
+      // this function allocates memory for the new node using the previous functions
       void * my_malloc(unsigned long size){
         
-        void *memory_block;
-        node *header;
+        void *memory_block; 
+        node *new_node;   
         pthread_mutex_lock(&lock);
         if(!size)
           return (NULL);
-        header = look_for_memory(size);
-        if(header != NULL) {
-          header->is_free = false;
-          pthread_mutex_unlock(&lock);
-          return (void*)(header + 1);
+        new_node = look_for_memory(size); // look for the memory using the look_for_memory function
+        if(new_node != NULL) { 
+          new_node->is_free = false; // assign the status of the node to false
+          pthread_mutex_unlock(&lock); // unlock the mutex
+          return (void*)(new_node + 1); // return pointer to the node
         }
-        memory_block = sbrk((sizeof(node) + size));
-        if(memory_block == (void*) - 1) {
+        memory_block = sbrk((sizeof(node) + size)); // allocate memory using sbrk function
+        if(memory_block == (void*) - 1) { // if the allocation fails
           pthread_mutex_unlock(&lock);
           return (NULL);
         }
-        header = update_node(header, size, memory_block);
-        return (void*)(header + 1);
+        new_node = update_node(new_node, size, memory_block); // update the node using the update_node function
+        return (void*)(new_node + 1);
       }
         
-    
-
+    // this function allocate memory for number of nodes using my_malloc function
+void *my_calloc(int number, unsigned long size){
+  cout<<"Amen Calloc"<<endl;
+  return (my_malloc(number * size));
+}
  
-
+// this function frees the memory 
 void my_free(void *ptr)
 
 {
     node *new_node;
     void *new_brk;
-
- 
-
     if (!ptr)
 
         return;
 
-    pthread_mutex_lock(&lock);
+    pthread_mutex_lock(&lock);  // lock the mutex
 
     new_node = (node*)ptr - 1;
 
-    new_brk = sbrk(0);
+    new_brk = sbrk(0);  // get the current sbrk value
 
-    if ((char*)ptr + new_node->size == new_brk) {
+    if ((char*)ptr + new_node->size == new_brk) {   // if the node is the last node
 
-        if (start == end1) {
+        if (start == end1) { // if the start and end nodes are the same we assign the start and end nodes to NULL
 
             start = NULL;
 
             end1 = NULL;
 
-        } else {
+        } else { 
 
-            delete_pointers();
+            delete_pointers(); // delete the pointers using the delete_pointers function
 
         }
 
-        sbrk((0 - sizeof(node) - new_node->size));
+        sbrk((0 - sizeof(node) - new_node->size)); // free the memory using sbrk function
 
     } else {
 
-        new_node->is_free = true;
+        new_node->is_free = true; // assign the status of the node to true
 
     }
 
-    pthread_mutex_unlock(&lock);
+    pthread_mutex_unlock(&lock); // unlock the mutex
 
 }   
     
-
+// this function deletes the pointers
 void delete_pointers(void)
 
 {
 
     node *buff = start;
-
- 
-
     while (buff) {
 
         if (buff->next == end1) {
@@ -199,11 +203,6 @@ void delete_pointers(void)
 
     }
 }
-
-
-
-
  
 };
-
 
