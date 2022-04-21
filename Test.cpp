242439345e3b79e1358dae59da86539befea9f24
GuyAzoulay <87694635+GuyAzoulay@users.noreply.github.com>
2022-04-21 10:33:26 +0300

    Add files via upload

---
+++ Test.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+#include "stack.hpp"
+#include "doctest.h"
+#include <cstdlib>
+#include <pthread.h>
+using namespace std;
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+
+
+TEST_CASE("Test Stack") {
+    stack<string> stack;
+    stack.push("Hello");
+    stack.push("World");
+    stack.push("!");
+    CHECK(stack.top() == "!");
+    stack.pop(); 
+    CHECK(stack.top() == "World");
+    stack.pop();
+    CHECK(stack.top() == "Hello");
+    stack.pop();
+    CHECK(stack.empty());
+    CHECK(stack.top() == "Error- Stack is empty!");
+}
